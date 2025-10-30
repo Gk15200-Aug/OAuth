4ea21c5718e808df07547a4004b995b50876047a
Gk15200-Aug <74087598+Gk15200-Aug@users.noreply.github.com>
2025-10-31 03:45:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -36,11 +36,11 @@ app.use(cookieParser());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
-app.use(express.static(path.join(__dirname, "client", "build")));
+// app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 
 // Error handler middleware
 app.use((err, req, res, next) => {
